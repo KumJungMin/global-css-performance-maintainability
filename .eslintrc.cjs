fc3bd770843ace7409ee746a683e1734ae20cf0c
KumJungMin <today.as.fresh@gmail.com>
2025-04-21 11:00:51 +0900

    chore: setting eslint, prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: ['vue', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+  },
+};
