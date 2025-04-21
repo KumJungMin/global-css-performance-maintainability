f18047155d3ebdd3477ef2ca56ac36fa77b35bbb
KumJungMin <today.as.fresh@gmail.com>
2025-04-21 15:26:10 +0900

    chore: add vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+
 import './assets/base.css';
 import './assets/front.css';
 import './assets/layout.css';
 import './assets/reset.css';
 import './assets/setting-market.css';
 
-import { createApp } from 'vue';
-import App from './App.vue';
-
-createApp(App).mount('#app');
+createApp(App).use(router).mount('#app');
